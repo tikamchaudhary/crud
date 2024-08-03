c045a120c282ee80af158d94f8470b73de783255
tikamsingh172 <tikamsingh172@gmail.com>
2024-08-03 07:15:35 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/tikamchaudhary/crud.git
+git push -u origin main
